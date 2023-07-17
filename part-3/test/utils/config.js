90bcdba30e00e5fa37ed1aa1d4622f5177dc1d19
trysam2003 <trysam2003@gmail.com>
2023-07-17 15:28:19 +0100

    covert MONGODB_URI to string

--- part-3/test/utils/config.js
+++ part-3/test/utils/config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const { PORT } = process.env;
 const MONGODB_URI = process.env.MODE_ENV === 'test'
-  ? process.env.TEST_MONGODB_URL
-  : process.env.MONGODB_URI;
+  ? process.env.TEST_MONGODB_URL.toString()
+  : process.env.MONGODB_URI.toString();
 
 module.exports = {
   PORT,
